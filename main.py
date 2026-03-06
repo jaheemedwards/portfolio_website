6bf6edcff54de19f3acb6d1427d943c70417430c
Jaheem Edwards <jaheem@Jaheem-Laptop.>
2026-03-06 00:14:21 -0400

    Updated my resume

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def show_projects(projects, filter_tags=None):
 
 
 email = "jaheemedwardswork@gmail.com"
-resume_path = "assets/Resume with CL Jaheem Edwards.pdf"
+resume_path = "assets/Jaheem Edwards Resume.pdf"
 
 st.set_page_config(page_title="Portfolio", layout="wide")
 
